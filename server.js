f4616ba9642a651057c47cb1afec793576c71283
Mark John Alcantara <MarkAlcantara@Aprils-MacBook-Pro.local>
2021-11-04 20:09:28 +0800

    configuration changes for deployment in azure app service

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(3000, () => console.log("server is listening to port 3000"));
+app.listen(8080, () => console.log("server is listening to port 8080"));
